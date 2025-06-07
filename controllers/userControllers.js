66ca09796d8cefb97c47f9a866c9747481352c52
Cristopher <cristophervaldezd@gmail.com>
2025-06-07 19:13:20 -0400

    Create API routes and user controller

---
+++ controllers/userControllers.js
@@ -0,0 +1,87 @@
+const { User } = require('../models');
+
+module.exports = {
+  // GET all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find().populate('friends').populate('thoughts');
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // GET single user by _id
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findById(req.params.userId).populate('friends').populate('thoughts');
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // POST create user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // PUT update user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // DELETE user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.userId);
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.json({ message: 'User deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // POST add friend
+  async addFriend(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // DELETE remove friend
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
---
+++ models/users.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+// Email validation regex
+const validateEmail = function(email) {
+  const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property to count friends
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
